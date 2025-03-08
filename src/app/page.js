9763417f8f89b60f5617addd2395e55b148268e4
Ritu <ritu@cleardigital.com>
2025-03-08 19:07:33 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,11 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ColTwoGrid from './user/Component/ColTwoGrid';
-import CardWithGrid from './user/Component/CardWithGrid';
 const Home = () =>  {
   return (
     <>
    <ColTwoGrid/>
-   <CardWithGrid/>
+ 
     </>
    
   );
